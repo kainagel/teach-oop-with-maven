a48690771f9d069f01c59e0a6e973e3c413ba6c6
kainagel <nagel@vsp.tu-berlin.de>
2020-01-21 14:26:36 +0100

    changes

---
+++ src/main/java/de/tuberlin/vsp/teach/dijkstra/Main.java
@@ -0,0 +1,78 @@
+package de.tuberlin.vsp.teach.dijkstra;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.Node;
+
+import java.util.*;
+
+class Main{
+
+	public static void main( String[] args ){
+
+		Network network = null ;
+
+		Queue<DNode> pendingNodes = new PriorityQueue<>( new Comparator<DNode>(){
+			@Override public int compare( DNode o1, DNode o2 ){
+				return Double.compare( o1.getCost(), o2.getCost() ) ;
+			}
+		} ) ;
+
+		Map<Id<Node>,DNode> dnodes = new LinkedHashMap<>() ;
+		for( Node node : network.getNodes().values() ){
+			DNode dNode = new DNode( node ) ;
+			dNode.setCost( Double.POSITIVE_INFINITY );
+			dnodes.put(  node.getId(), dNode ) ;
+			pendingNodes.add(  dNode ) ;
+		}
+
+		Node startNode = null ;
+		Node destNode = null ;
+		dnodes.get(  startNode.getId() ).setCost( 0 );
+		// yyyy can we change sorting after insertion??
+
+		while (true){
+			DNode currentNode = pendingNodes.remove();
+			if ( currentNode.getNode().getId().equals( destNode.getId() ) ) {
+				break ;
+			}
+			double currentCost = currentNode.getCost();
+			for( Link link : currentNode.getNode().getOutLinks().values() ){
+				DNode nextDNode = dnodes.get( link.getToNode().getId() );
+				;
+				final double possibleCost = currentCost + link.getLength() / link.getFreespeed();
+				if( possibleCost < nextDNode.getCost() ){
+					nextDNode.setCost( possibleCost );
+					nextDNode.setPrevNode( currentNode );
+				}
+			}
+		}
+
+	}
+
+	static class DNode {
+		private final Node node;
+		private double cost ;
+		private DNode prevNode;
+		DNode( Node node ) {
+			this.node = node ;
+		}
+		Node getNode(){
+			return node;
+		}
+		double getCost(){
+			return cost;
+		}
+		void setCost( double cost ){
+			this.cost = cost;
+		}
+		void setPrevNode( DNode prevNode ){
+			this.prevNode = prevNode;
+		}
+		public DNode getPrevNode(){
+			return prevNode;
+		}
+	}
+
+}
--- src/main/java/de/tuberlin/vsp/teach/processing/Example3D.java
+++ src/main/java/de/tuberlin/vsp/teach/processing/Example3D.java
@@ -1,55 +1,63 @@
-package de.tuberlin.vsp.teach.processing;
-
-import processing.core.PApplet;
-
-public class Example3D extends PApplet {
-	private final int xsize=600, ysize=600 ;
-
-	private double xx=xsize/2 ;
-	private double yy=ysize/2 ;
-	private double dx=Math.random() ;
-	private double dy=Math.random() ;
-
-	public static void main( String[] args ) {
-		PApplet.main( new String[] { "--present", "de.tuberlin.vsp.teach.processing.Example3D"}  );
-	}
-
-	@Override
-	public void draw() {
-		background(0) ;
-		lights() ;
-		
-		noStroke();
-
-		pushMatrix();
-		translate( (float) xx, (float) yy, 0);
-		rotateY(1.25f);
-		rotateX(-0.4f);
-		box(100);
-		popMatrix();
-
-		xx += dx ;
-		yy += dy ;
-
-		if ( xx >= xsize ) {
-			dx = - 10.*pow( (float) Math.random(),2 ) - 1 ;
-		}
-		if ( xx <=0 ) {
-			dx = 10.*pow( (float) Math.random(), 2 ) + 1 ;
-		}
-		if ( yy >= ysize ) {
-			dy = - 10.*pow( (float) Math.random(), 2 ) - 1 ;
-		}
-		if ( yy <= 0 ) {
-			dy = 10.* pow( (float) Math.random(), 2 ) + 1 ;
-		}
-
-	}
-	
-	@Override
-	public void settings() { // setup does not work here when not using the PDE
-		size(xsize,ysize, P3D );
-	}
-
-
-}
+//package de.tuberlin.vsp.teach.processing;
+//
+//import processing.core.PApplet;
+//
+//public class Example3D extends PApplet {
+//	private double xx=10. ;
+//	private double yy=10. ;
+//	private double dx=Math.random() + 10. ;
+//	private double dy=Math.random() + 10. ;
+//
+//	public static void main( String[] args ) {
+//		PApplet.main( new String[] { "--present", "de.tuberlin.vsp.teach.processing.Example3D"}  );
+//	}
+//
+//	float ii  = 0 ;
+//	final double radius = 50. ;
+//
+//	@Override
+//	public void draw() {
+//		ii+=1;
+//
+//		background(127) ;
+//		lights() ;
+//
+//		noStroke();
+//		this.fill(255, 123, 0, 255);
+//
+////		ortho(-width/2, width/2, -height/2, height/2);
+//
+//		pushMatrix();
+//		translate( (float) xx, (float) yy, -ii);
+////		rotateY(1.25f);
+////		rotateX(-0.4f);
+////		rotateZ(ii) ;
+//		box(100);
+////		this.sphere((float) radius);
+//		popMatrix();
+//
+//		xx += dx ;
+//		yy += dy ;
+//
+//		if ( xx >= width-radius ) {
+//			dx = - 10.*Math.pow( Math.random(),2 ) - 1 ;
+//		}
+//		if ( xx <=0+radius ) {
+//			dx = 10.*Math.pow( Math.random(), 2 ) + 1 ;
+//		}
+//		if ( yy >= height-radius ) {
+//			dy = - 10.*Math.pow( Math.random(), 2 ) - 1 ;
+//		}
+//		if ( yy <= 0+radius ) {
+//			dy = 10.* Math.pow( Math.random(), 2 ) + 1 ;
+//		}
+//
+//	}
+//
+//	@Override
+//	public void settings() { // setup does not work here when not using the PDE
+//		this.fullScreen(P3D);
+//	}
+//
+//
+//}
