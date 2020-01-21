21544b8731434bb7fb42da87ab87441fedc93286
kainagel <nagel@vsp.tu-berlin.de>
2020-01-21 15:17:50 +0100

    remove dep on matsim; modify geotools example

--- src/main/java/de/tuberlin/vsp/teach/geotools/Main.java
+++ src/main/java/de/tuberlin/vsp/teach/geotools/Main.java
@@ -1,20 +1,5 @@
 package de.tuberlin.vsp.teach.geotools;
 
-import org.geotools.data.FeatureReader;
-import org.geotools.data.FileDataStore;
-import org.geotools.data.FileDataStoreFinder;
-import org.geotools.data.simple.SimpleFeatureIterator;
-import org.geotools.data.simple.SimpleFeatureSource;
-import org.matsim.core.utils.io.UncheckedIOException;
-import org.opengis.feature.simple.SimpleFeature;
-import org.opengis.feature.simple.SimpleFeatureType;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 class Main{
 
         public static void main( String[] args ){
